871abe42b1695b97ac1f2519a2add774f062247e
Sergey Fayman <sergfa@gmai.com>
2021-10-17 23:02:46 +0300

    fix deploy

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -7,19 +7,19 @@ module.exports = function(deployer) {
     let firstAirlineAddress = '0x5FfF5C8c4BCfBE1c67EF487Ca7911062f05aa1dE';
     let firstAirlineName = 'ELAL';
     
-    deployer.deploy(flightSuretyData, firstAirlineAddress, firstAirlineName);
-   /* .then(() => {
-        return deployer.deploy(FlightSuretyApp(FlightSuretyData.address))
+    deployer.deploy(flightSuretyData, firstAirlineAddress, firstAirlineName)
+    .then(() => {
+        return deployer.deploy(flightSuretyApp,flightSuretyData.address)
                 .then(() => {
                     let config = {
                         localhost: {
                             url: 'http://localhost:8545',
-                            dataAddress: FlightSuretyData.address,
-                            appAddress: FlightSuretyApp.address
+                            dataAddress: flightSuretyData.address,
+                            appAddress: flightSuretyApp.address
                         }
                     }
                     fs.writeFileSync(__dirname + '/../src/dapp/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
                     fs.writeFileSync(__dirname + '/../src/server/config.json',JSON.stringify(config, null, '\t'), 'utf-8');
                 });
-    });*/
+    });
 }
